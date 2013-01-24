184740ad8b3c10ffe5d8dd3547f3262a47ff2069
Chris Ferdinandi <hello@gomakethings.com>
2013-01-24 10:03:56 -0500

    Initial Release

---
+++ houdini.js
@@ -0,0 +1,39 @@
+/* =============================================================
+ * houdini.js v1.0.0
+ * A simple collapse and expand widget.
+ * Script by Chris Ferdinandi - http://gomakethings.com
+ * Licensed under WTFPL - http://www.wtfpl.net/
+ * ============================================================= */
+
+$(function () {
+    $('.collapse-toggle').click(function(e) { // When a link or button with the .collapse-toggle class is clicked
+        e.preventDefault(); // Prevent the default action from occurring
+
+        // Set Variables
+        var dataID = $(this).attr('data-target'); // dataID is the data-target value
+        var hrefID = $(this).attr('href'); // hrefID is the href value
+
+        // Toggle the Active Class
+        if ($(this).attr('data-target'))  { // If the clicked element has a data-target
+            $(dataID).toggleClass('active'); // Add or remove the .active class from the element whose ID matches the data-target value
+        }
+        else { // Otherwise
+            $(hrefID).toggleClass('active'); // Add or remove the .active class from the element whose ID matches the href value
+        }
+    });
+});
+
+
+
+
+
+/* =============================================================
+ * js-accessibility.js v1.0.0
+ * Adds .js class to <body> for progressive enhancement.
+ * Script by Chris Ferdinandi - http://gomakethings.com
+ * Licensed under WTFPL - http://www.wtfpl.net
+ * ============================================================= */
+
+$(function () {
+    $('body').addClass('js'); // On page load, add the .js class to the <body> element.
+});
