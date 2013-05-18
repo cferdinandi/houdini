2c012c222c4adea5f12ad3d68a9869a0a382d321
Chris Ferdinandi <hello@gomakethings.com>
2013-05-17 22:43:15 -0400

    Set jQuery to noConflict mode for better compatability

--- houdini.js
+++ houdini.js
@@ -1,39 +1,43 @@
 /* =============================================================
- * houdini.js v1.0.0
+ * houdini.js
  * A simple collapse and expand widget.
  * Script by Chris Ferdinandi - http://gomakethings.com
  * Licensed under WTFPL - http://www.wtfpl.net/
  * ============================================================= */
 
-$(function () {
-    $('.collapse-toggle').click(function(e) { // When a link or button with the .collapse-toggle class is clicked
-        e.preventDefault(); // Prevent the default action from occurring
+(function($) {
+    $(function () {
+        $('.collapse-toggle').click(function(e) { // When a link or button with the .collapse-toggle class is clicked
+            e.preventDefault(); // Prevent the default action from occurring
 
-        // Set Variables
-        var dataID = $(this).attr('data-target'); // dataID is the data-target value
-        var hrefID = $(this).attr('href'); // hrefID is the href value
+            // Set Variables
+            var dataID = $(this).attr('data-target'); // dataID is the data-target value
+            var hrefID = $(this).attr('href'); // hrefID is the href value
 
-        // Toggle the Active Class
-        if (dataID)  { // If the clicked element has a data-target
-            $(dataID).toggleClass('active'); // Add or remove the .active class from the element whose ID matches the data-target value
-        }
-        else { // Otherwise
-            $(hrefID).toggleClass('active'); // Add or remove the .active class from the element whose ID matches the href value
-        }
+            // Toggle the Active Class
+            if (dataID)  { // If the clicked element has a data-target
+                $(dataID).toggleClass('active'); // Add or remove the .active class from the element whose ID matches the data-target value
+            }
+            else { // Otherwise
+                $(hrefID).toggleClass('active'); // Add or remove the .active class from the element whose ID matches the href value
+            }
+        });
     });
-});
+})(jQuery);
 
 
 
 
 
 /* =============================================================
- * js-accessibility.js v1.0.0
+ * js-accessibility.js
  * Adds .js class to <body> for progressive enhancement.
  * Script by Chris Ferdinandi - http://gomakethings.com
  * Licensed under WTFPL - http://www.wtfpl.net
  * ============================================================= */
 
-$(function () {
-    $('body').addClass('js'); // On page load, add the .js class to the <body> element.
-});
+(function($) {
+    $(function () {
+        $('body').addClass('js'); // On page load, add the .js class to the <body> element.
+    });
+})(jQuery);
