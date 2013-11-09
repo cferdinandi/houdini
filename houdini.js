e1f8e92cefa1abf8257a0c49f70bfb7f52400b9d
Chris Ferdinandi <hello@gomakethings.com>
2013-11-09 11:22:51 -0500

    Converted from spaces to tabs
    http://lea.verou.me/2012/01/why-tabs-are-clearly-superior/

--- houdini.js
+++ houdini.js
@@ -1,53 +1,53 @@
 /* =============================================================
 
-    Houdini v3.4
-    A simple collapse and expand widget by Chris Ferdinandi.
-    http://gomakethings.com
+	Houdini v3.4
+	A simple collapse and expand widget by Chris Ferdinandi.
+	http://gomakethings.com
 
-    Free to use under the MIT License.
-    http://gomakethings.com/mit/
-    
+	Free to use under the MIT License.
+	http://gomakethings.com/mit/
+
  * ============================================================= */
 
 (function() {
 
-    'use strict';
+	'use strict';
 
-    // Feature Test
-    if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
+	// Feature Test
+	if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
 
-        // Function to toggle collapse/expand widget
-        var toggleCollapse = function (toggle) {
+		// Function to toggle collapse/expand widget
+		var toggleCollapse = function (toggle) {
 
-            // Define the content container
-            var dataID = toggle.getAttribute('data-target');
-            var dataTarget = document.querySelector(dataID);
+			// Define the content container
+			var dataID = toggle.getAttribute('data-target');
+			var dataTarget = document.querySelector(dataID);
 
-            // Toggle the '.active' class on the toggle and container elements
-            buoy.toggleClass(toggle, 'active');
-            buoy.toggleClass(dataTarget, 'active');
+			// Toggle the '.active' class on the toggle and container elements
+			buoy.toggleClass(toggle, 'active');
+			buoy.toggleClass(dataTarget, 'active');
 
-        };
+		};
 
-        // Define collapse toggle
-        var collapseToggle = document.querySelectorAll('.collapse-toggle');
+		// Define collapse toggle
+		var collapseToggle = document.querySelectorAll('.collapse-toggle');
 
-        // For each collapse toggle
-        [].forEach.call(collapseToggle, function (toggle) {
+		// For each collapse toggle
+		[].forEach.call(collapseToggle, function (toggle) {
 
-            // When the toggle is clicked
-            toggle.addEventListener('click', function(e) {
+			// When the toggle is clicked
+			toggle.addEventListener('click', function(e) {
 
-                // Prevent default link behavior
-                e.preventDefault();
+				// Prevent default link behavior
+				e.preventDefault();
 
-                // Toggle the collapse/expand widget
-                toggleCollapse(toggle);
-                
-            }, false);
+				// Toggle the collapse/expand widget
+				toggleCollapse(toggle);
 
-        });
+			}, false);
 
-    }
+		});
+
+	}
 
 })();
