3563602d8cd37b33cbd6decc27a71a09d27593b2
Chris Ferdinandi <hello@gomakethings.com>
2014-02-22 18:34:23 -0500

    Convert `toggleCollapse` to a public method

--- js/houdini.js
+++ js/houdini.js
@@ -16,7 +16,7 @@ window.houdini = (function (window, document, undefined) {
 	// Stop YouTube, Vimeo, and HTML5 videos from playing when leaving the slide
 	// Private method
 	// Runs functions
-	var _stopVideo = function (content) {
+	var _stopVideos = function (content) {
 		if ( !buoy.hasClass(content, 'active') ) {
 			var iframe = content.querySelector( 'iframe');
 			var video = content.querySelector( 'video' );
@@ -31,43 +31,43 @@ window.houdini = (function (window, document, undefined) {
 	};
 
 	// Toggle the collapse/expand widget
-	// Private method
+	// Public method
 	// Runs functions
-	var _toggleCollapse = function (event) {
+	var toggleCollapse = function (toggle, contentID, event) {
 
-		// Selectors and variables
-		var dataID = this.getAttribute('data-collapse');
-		var dataTarget = document.querySelector(dataID);
+		var content = document.querySelector(contentID); // Get content area
 
 		// If a link, prevent default click event
-		if ( this!== null && this !== undefined && this.tagName == 'A' ) {
+		if ( toggle !== null && toggle !== undefined && toggle.tagName == 'A' ) {
 			if ( event !== undefined && event !== null ) {
 				event.preventDefault();
 			}
 		}
 
 		// Toggle collapse element
-		buoy.toggleClass(this, 'active'); // Change text on collapse toggle
-		buoy.toggleClass(dataTarget, 'active'); // Collapse or expand content area
-		_stopVideo(dataTarget); // If content area is closed, stop playing any videos
+		buoy.toggleClass(toggle, 'active'); // Change text on collapse toggle
+		buoy.toggleClass(content, 'active'); // Collapse or expand content area
+		_stopVideos(content); // If content area is closed, stop playing any videos
 
 	};
 
 	// Initialize Houdini
 	// Public method
 	// Runs functions
-	var init = function () {
+	var init = function ( options ) {
 
 		// Feature test before initilizing
 		if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
 
 			// Selectors and variables
 			var toggles = document.querySelectorAll('[data-collapse]'); // Get all collapse toggles
-			buoy.addClass(document.documentElement, 'js-houdini'); // Add class to HTML element to activate conditional CSS
+
+			// Add class to HTML element to activate conditional CSS
+			buoy.addClass(document.documentElement, 'js-houdini');
 
 			// Whenever a toggle is clicked, run the expand/collapse function
 			Array.prototype.forEach.call(toggles, function (toggle, index) {
-				toggle.addEventListener('click', _toggleCollapse, false);
+				toggle.addEventListener('click', _toggleCollapse.bind( null, toggle, toggle.getAttribute('data-collapse') ), false);
 			});
 
 		}
@@ -76,7 +76,8 @@ window.houdini = (function (window, document, undefined) {
 
 	// Return public methods
 	return {
-		init: init
+		init: init,
+		toggleCollapse: toggleCollapse
 	};
 
 })(window, document);
