290ac823797b4f1f25cf4ad927f7083c59cd6367
Chris Ferdinandi <hello@gomakethings.com>
2014-04-04 09:43:09 -0400

    Updated Buoy
    Converted from Buoy class helpers to `classList` with polyfill.

--- js/houdini.js
+++ js/houdini.js
@@ -1,6 +1,6 @@
 /* =============================================================
 
-	Houdini v5.3
+	Houdini v6.0
 	A simple collapse and expand widget by Chris Ferdinandi.
 	http://gomakethings.com
 
@@ -36,8 +36,8 @@ window.houdini = (function (window, document, undefined) {
 	// Stop YouTube, Vimeo, and HTML5 videos from playing when leaving the slide
 	// Private method
 	// Runs functions
-	var _stopVideos = function (content) {
-		if ( !buoy.hasClass(content, 'active') ) {
+	var _stopVideos = function ( content, activeClass ) {
+		if ( !content.classList.contains( activeClass ) ) {
 			var iframe = content.querySelector( 'iframe');
 			var video = content.querySelector( 'video' );
 			if ( iframe !== null ) {
@@ -54,7 +54,7 @@ window.houdini = (function (window, document, undefined) {
 	// Private method
 	// Runs functions
 	var _closeCollapseGroup = function ( toggle, options ) {
-		if ( !buoy.hasClass(toggle, options.toggleActiveClass) && toggle.hasAttribute('data-group') ) {
+		if ( !toggle.classList.contains( options.toggleActiveClass ) && toggle.hasAttribute('data-group') ) {
 
 			// Get all toggles in the group
 			var groupName = toggle.getAttribute('data-group');
@@ -63,8 +63,8 @@ window.houdini = (function (window, document, undefined) {
 			// Deactivate each toggle and it's content area
 			Array.prototype.forEach.call(group, function (item, index) {
 				var content = document.querySelector( item.getAttribute('data-collapse') );
-				buoy.removeClass(item, options.toggleActiveClass);
-				buoy.removeClass(content, options.contentActiveClass);
+				item.classList.remove( options.toggleActiveClass );
+				content.classList.remove( options.contentActiveClass );
 			});
 
 		}
@@ -87,9 +87,9 @@ window.houdini = (function (window, document, undefined) {
 
 		// Toggle collapse element
 		_closeCollapseGroup(toggle, options); // Close collapse group items
-		buoy.toggleClass(toggle, options.toggleActiveClass); // Change text on collapse toggle
-		buoy.toggleClass(content, options.contentActiveClass); // Collapse or expand content area
-		_stopVideos(content); // If content area is closed, stop playing any videos
+		toggle.classList.toggle( options.toggleActiveClass );// Change text on collapse toggle
+		content.classList.toggle( options.contentActiveClass ); // Collapse or expand content area
+		_stopVideos( content, options.contentActiveClass ); // If content area is closed, stop playing any videos
 
 		options.callbackAfter( toggle, contentID ); // Run callbacks after toggling content
 
@@ -108,7 +108,7 @@ window.houdini = (function (window, document, undefined) {
 			var toggles = document.querySelectorAll('[data-collapse]'); // Get all collapse toggles
 
 			// Add class to HTML element to activate conditional CSS
-			buoy.addClass(document.documentElement, options.initClass);
+			document.documentElement.classList.add( options.initClass );
 
 			// Whenever a toggle is clicked, run the expand/collapse function
 			Array.prototype.forEach.call(toggles, function (toggle, index) {
