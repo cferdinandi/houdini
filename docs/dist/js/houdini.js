e781e4b820dc2ac0f6e7edf1ac25a9da474bec9f
Chris Ferdinandi <hello@gomakethings.com>
2014-10-17 22:13:00 -0400

    Create docs

---
+++ docs/dist/js/houdini.js
@@ -0,0 +1,232 @@
+/**
+ * Houdini v6.4.3
+ * A simple collapse-and-expand script., by Chris Ferdinandi.
+ * http://github.com/cferdinandi/houdini
+ * 
+ * Free to use under the MIT License.
+ * http://gomakethings.com/mit/
+ */
+
+(function (root, factory) {
+	if ( typeof define === 'function' && define.amd ) {
+		define('houdini', factory(root));
+	} else if ( typeof exports === 'object' ) {
+		module.exports = factory(root);
+	} else {
+		root.houdini = factory(root);
+	}
+})(window || this, function (root) {
+
+	'use strict';
+
+	//
+	// Variables
+	//
+
+	var houdini = {}; // Object for public APIs
+	var supports = !!document.querySelector && !!root.addEventListener; // Feature test
+	var settings;
+
+	// Default settings
+	var defaults = {
+		toggleActiveClass: 'active',
+		contentActiveClass: 'active',
+		initClass: 'js-houdini',
+		callbackBefore: function () {},
+		callbackAfter: function () {}
+	};
+
+
+	//
+	// Methods
+	//
+
+	/**
+	 * A simple forEach() implementation for Arrays, Objects and NodeLists
+	 * @private
+	 * @param {Array|Object|NodeList} collection Collection of items to iterate
+	 * @param {Function} callback Callback function for each iteration
+	 * @param {Array|Object|NodeList} scope Object/NodeList/Array that forEach is iterating over (aka `this`)
+	 */
+	var forEach = function (collection, callback, scope) {
+		if (Object.prototype.toString.call(collection) === '[object Object]') {
+			for (var prop in collection) {
+				if (Object.prototype.hasOwnProperty.call(collection, prop)) {
+					callback.call(scope, collection[prop], prop, collection);
+				}
+			}
+		} else {
+			for (var i = 0, len = collection.length; i < len; i++) {
+				callback.call(scope, collection[i], i, collection);
+			}
+		}
+	};
+
+	/**
+	 * Merge defaults with user options
+	 * @private
+	 * @param {Object} defaults Default settings
+	 * @param {Object} options User options
+	 * @returns {Object} Merged values of defaults and options
+	 */
+	var extend = function ( defaults, options ) {
+		var extended = {};
+		forEach(defaults, function (value, prop) {
+			extended[prop] = defaults[prop];
+		});
+		forEach(options, function (value, prop) {
+			extended[prop] = options[prop];
+		});
+		return extended;
+	};
+
+	/**
+	 * Get the closest matching element up the DOM tree
+	 * @param {Element} elem Starting element
+	 * @param {String} selector Selector to match against (class, ID, or data attribute)
+	 * @return {Boolean|Element} Returns false if not match found
+	 */
+	var getClosest = function (elem, selector) {
+		var firstChar = selector.charAt(0);
+		for ( ; elem && elem !== document; elem = elem.parentNode ) {
+			if ( firstChar === '.' ) {
+				if ( elem.classList.contains( selector.substr(1) ) ) {
+					return elem;
+				}
+			} else if ( firstChar === '#' ) {
+				if ( elem.id === selector.substr(1) ) {
+					return elem;
+				}
+			} else if ( firstChar === '[' ) {
+				if ( elem.hasAttribute( selector.substr(1, selector.length - 2) ) ) {
+					return elem;
+				}
+			}
+		}
+		return false;
+	};
+
+	/**
+	 * Stop YouTube, Vimeo, and HTML5 videos from playing when leaving the slide
+	 * @private
+	 * @param  {Element} content The content container the video is in
+	 * @param  {String} activeClass The class asigned to expanded content areas
+	 */
+	var stopVideos = function ( content, activeClass ) {
+		if ( !content.classList.contains( activeClass ) ) {
+			var iframe = content.querySelector( 'iframe');
+			var video = content.querySelector( 'video' );
+			if ( iframe ) {
+				var iframeSrc = iframe.src;
+				iframe.src = iframeSrc;
+			}
+			if ( video ) {
+				video.pause();
+			}
+		}
+	};
+
+	/**
+	 * Close all content areas in an expand/collapse group
+	 * @private
+	 * @param  {Element} toggle The element that toggled the expand or collapse
+	 * @param  {Object} settings
+	 */
+	var closeCollapseGroup = function ( toggle, settings ) {
+		if ( !toggle.classList.contains( settings.toggleActiveClass ) && toggle.hasAttribute('data-group') ) {
+
+			// Get all toggles in the group
+			var groupName = toggle.getAttribute('data-group');
+			var group = document.querySelectorAll('[data-group="' + groupName + '"]');
+
+			// Deactivate each toggle and it's content area
+			forEach(group, function (item) {
+				var content = document.querySelector( item.getAttribute('data-collapse') );
+				item.classList.remove( settings.toggleActiveClass );
+				content.classList.remove( settings.contentActiveClass );
+			});
+
+		}
+	};
+
+	/**
+	 * Toggle the collapse/expand widget
+	 * @public
+	 * @param  {Element} toggle The element that toggled the expand or collapse
+	 * @param  {String} contentID The ID of the content area to expand or collapse
+	 * @param  {Object} options
+	 * @param  {Event} event
+	 */
+	houdini.toggleContent = function (toggle, contentID, options) {
+
+		var settings = extend( settings || defaults, options || {} );  // Merge user options with defaults
+		var content = document.querySelector(contentID); // Get content area
+
+		settings.callbackBefore( toggle, contentID ); // Run callbacks before toggling content
+
+		// Toggle collapse element
+		closeCollapseGroup(toggle, settings); // Close collapse group items
+		toggle.classList.toggle( settings.toggleActiveClass );// Change text on collapse toggle
+		content.classList.toggle( settings.contentActiveClass ); // Collapse or expand content area
+		stopVideos( content, settings.contentActiveClass ); // If content area is closed, stop playing any videos
+
+		settings.callbackAfter( toggle, contentID ); // Run callbacks after toggling content
+
+	};
+
+	/**
+	 * Handle toggle click events
+	 * @private
+	 */
+	var eventHandler = function (event) {
+		var toggle = getClosest(event.target, '[data-collapse]');
+		if ( toggle ) {
+			event.preventDefault();
+			var contentID = toggle.hasAttribute('data-collapse') ? toggle.getAttribute('data-collapse') : toggle.parentNode.getAttribute('data-collapse');
+			houdini.toggleContent( toggle, contentID, settings );
+		}
+	};
+
+	/**
+	 * Destroy the current initialization.
+	 * @public
+	 */
+	houdini.destroy = function () {
+		if ( !settings ) return;
+		document.documentElement.classList.remove( settings.initClass );
+		document.removeEventListener('click', eventHandler, false);
+		settings = null;
+	};
+
+	/**
+	 * Initialize Houdini
+	 * @public
+	 * @param {Object} options User settings
+	 */
+	houdini.init = function ( options ) {
+
+		// feature test
+		if ( !supports ) return;
+
+		// Destroy any existing initializations
+		houdini.destroy();
+
+		// Merge user options with defaults
+		settings = extend( defaults, options || {} );
+
+		// Add class to HTML element to activate conditional CSS
+		document.documentElement.classList.add( settings.initClass );
+
+		// Listen for all click events
+		document.addEventListener('click', eventHandler, false);
+
+	};
+
+
+	//
+	// Public APIs
+	//
+
+	return houdini;
+
+});
