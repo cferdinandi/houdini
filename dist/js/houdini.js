c13ee34af71626cf37c97b7719412d4e633d9d45
cferdinandi <chris@gomakethings.com>
2017-04-07 10:31:45 -0400

    v9.4.0 Added support for nested expand-and-collapse sections

--- dist/js/houdini.js
+++ dist/js/houdini.js
@@ -1,5 +1,5 @@
 /*!
- * Houdini v9.3.0: A simple collapse-and-expand script
+ * Houdini v9.4.0: A simple collapse-and-expand script
  * (c) 2017 Chris Ferdinandi
  * MIT License
  * http://github.com/cferdinandi/houdini
