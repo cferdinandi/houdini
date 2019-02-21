d6a76a2d8557cb4c0b75cd18255c033e8931d6c8
cferdinandi <chris@gomakethings.com>
2019-02-21 10:49:08 -0500

    Removed old files

--- dist/js/houdini.js
+++ dist/js/houdini.js
@@ -1,7 +1,7 @@
 /*!
  * houdinijs v11.0.0
  * A simple collapse-and-expand script
- * (c) 2018 Chris Ferdinandi
+ * (c) 2019 Chris Ferdinandi
  * MIT License
  * http://github.com/cferdinandi/houdini
  */
